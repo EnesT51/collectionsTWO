68f95180c4ad6e026c06b0aa191dff29380a9641
EnesT51 <99040669@mydavinci.nl>
2022-03-14 13:30:01 +0100

    opdracht met succes afgerond

---
+++ password.py
@@ -0,0 +1,18 @@
+import random
+import string 
+
+lowercase = [random.choice(string.ascii_lowercase) for e in range(3)] # hier worden 3 random letters gekozen.
+lowercase1 = [random.choice(string.ascii_lowercase) for e in range(4)] # 
+uppercase = [random.choice(string.ascii_uppercase) for i in range(random.randint(2,6))]
+digit = [random.choice(string.digits) for e in range(random.randint(4,7))]
+symbols = [random.choice(string.punctuation) for y in range(3)]
+
+shuffle1 = lowercase1 + uppercase + digit + symbols
+random.shuffle(shuffle1)
+shuffle_L_U = lowercase + shuffle1
+if len(shuffle_L_U) <24:
+    totaal = 24 - len(shuffle_L_U)
+    for i in range(totaal):
+        shuffle_L_U.append(random.choice(string.ascii_lowercase))
+
+print(''.join(shuffle_L_U))
