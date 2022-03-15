95e0851f9ebb3827f4b9abd321832648aa10c1d6
EnesT51 <99040669@mydavinci.nl>
2022-03-15 17:05:05 +0100

    klein aanpassing gedaan. opdracht afgerond!

--- password.py
+++ password.py
@@ -1,8 +1,8 @@
 import random
-import string 
+import string
 
-lowercase = [random.choice(string.ascii_lowercase) for e in range(3)] # hier worden 3 random letters gekozen.
-lowercase1 = [random.choice(string.ascii_lowercase) for e in range(4)] # 
+lowercase = [random.choice(string.ascii_lowercase) for e in range(3)]
+lowercase1 = [random.choice(string.ascii_lowercase) for e in range(4)] 
 uppercase = [random.choice(string.ascii_uppercase) for i in range(random.randint(2,6))]
 digit = [random.choice(string.digits) for e in range(random.randint(4,7))]
 symbols = [random.choice(string.punctuation) for y in range(3)]
@@ -10,8 +10,9 @@ symbols = [random.choice(string.punctuation) for y in range(3)]
 shuffle1 = lowercase1 + uppercase + digit + symbols
 random.shuffle(shuffle1)
 shuffle_L_U = lowercase + shuffle1
-if len(shuffle_L_U) <24:
-    totaal = 24 - len(shuffle_L_U)
+
+if len(shuffle_L_U) <26:
+    totaal = 26 - len(shuffle_L_U)
     for i in range(totaal):
         shuffle_L_U.append(random.choice(string.ascii_lowercase))
 
